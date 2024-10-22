4d6ed5e712457df308ff75cbb72310788e94700e
TNTSuperMan <tntsuperga@gmail.com>
2024-10-22 15:25:35 +0900

    fix config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import terser from "@rollup/plugin-terser"
 import ts from "@rollup/plugin-typescript"
 export default {
-    input: "src/entries/esmodule.ts",
+    input: "src/entries/module.ts",
     output: [{
-        file:"dist/esmodule.js",
+        file:"dist/module.js",
         plugins: [ terser() ]
     }],
     plugins: [
